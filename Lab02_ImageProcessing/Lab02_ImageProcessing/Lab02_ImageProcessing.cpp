05839f7a059a990af8aad570e3d0ba06fb2343f0
hyojeonglee <hyon2224@naver.com>
2017-11-30 12:12:27 +0900

    rename files

---
+++ Lab02_ImageProcessing/Lab02_ImageProcessing/Lab02_ImageProcessing.cpp
@@ -0,0 +1,80 @@
+// 영상처리.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <windows.h>
+#define BYTE unsigned char 
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	FILE *infile;
+	infile=fopen("Mandrill_g.bmp", "rb");
+	if(infile==NULL) { printf("There is no file!!!\n"); exit(1); }
+
+	BITMAPFILEHEADER hf;
+	fread(&hf,sizeof(BITMAPFILEHEADER),1,infile); // 파일헤드를 읽음 
+	if(hf.bfType!=0x4D42) exit(1); 
+
+	BITMAPINFOHEADER hInfo;
+	fread(&hInfo,sizeof(BITMAPINFOHEADER),1,infile); // 영상헤드를 읽음 
+	printf("Image Size: (%3dx%3d)\n",hInfo.biWidth,hInfo.biHeight);
+	printf("Pallete Type: %dbit Colors\n",hInfo.biBitCount);
+
+
+	// 256칼라 이하의 경우는 취급하지 않음
+	if(hInfo.biBitCount<8 ) { printf("Bad File format!!"); exit(1); }
+	RGBQUAD *pRGB; 
+	if(hInfo.biClrUsed!=0) // 팔레트가 있는 경우 
+	{
+		pRGB= new RGBQUAD [hInfo.biClrUsed]; // 팔레트의 크기만큼 메모리를 할당함 
+		fread(pRGB,sizeof(RGBQUAD),hInfo.biClrUsed,infile); // 팔레트를 파일에서 읽음 
+	}
+
+	// 영상데이타를 저장할 메모리 할당 
+
+	BYTE lpImg2[256][256][3];
+	fread(lpImg2,sizeof(char),hInfo.biSizeImage,infile); 
+	fclose(infile);
+	BYTE lpOutImg2[256][256][3];
+
+	// 작은 사각형 데이터
+	BYTE lpImg3[256][256][3];
+
+	for(int i=0; i<hInfo.biHeight; i++)
+		for(int j=0; j<hInfo.biWidth; j++) 
+			for (int c=0; c<3; c++)
+				lpImg3[i][j][c] = 255;
+	
+	for(int i=50; i<hInfo.biHeight-50; i++)
+		for(int j=50; j<hInfo.biWidth-50; j++) 
+			for (int c=0; c<3; c++)
+				lpImg3[i][j][c] = 0;
+	
+	for(int i=55; i<hInfo.biHeight-55; i++)
+		for(int j=55; j<hInfo.biWidth-55; j++) 
+			for (int c=0; c<3; c++)
+				lpImg3[i][j][c] = 255;
+	
+
+
+	// 코드 수정 부분
+
+	for(int i=0; i<hInfo.biHeight; i++)
+		for(int j=0; j<hInfo.biWidth; j++) 
+			for (int c=0; c<3; c++)
+				lpOutImg2[i][j][c] = lpImg2[i][j][c] * lpImg3[i][j][c];
+
+
+	///////////////////////////////////////////////////////////////////
+
+	// 영상 출력 (24비트인 트루칼라로 출력) 
+	FILE *outfile = fopen("3_이효정.bmp","wb");
+	fwrite(&hf,sizeof(char),sizeof(BITMAPFILEHEADER),outfile);
+	fwrite(&hInfo,sizeof(char),sizeof(BITMAPINFOHEADER),outfile);
+	fwrite(lpOutImg2,sizeof(char),3*256*256, outfile); 
+	fclose(outfile);
+
+	return 0;
+}
+
